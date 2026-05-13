98bbf045ffdb9e0a3615dd0775732a046dc0e377
Leo Pahl <leo.pahl@tum.de>
2026-05-13 19:55:29 +0200

    fix: update HealthController to return simple health status

--- services/user-progress/src/main/java/com/tso/userprogress/health/HealthController.java
+++ services/user-progress/src/main/java/com/tso/userprogress/health/HealthController.java
@@ -1,17 +1,15 @@
 package com.tso.userprogress.health;
 
-import com.tso.userprogress.api.UserProgressApi;
-import com.tso.userprogress.model.HealthStatus;
+
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-class HealthController implements UserProgressApi {
+class HealthController {
 
-  @Override
-  public ResponseEntity<HealthStatus> userProgressHealth() {
-    HealthStatus body =
-        new HealthStatus().status(HealthStatus.StatusEnum.OK).service("user-progress");
-    return ResponseEntity.ok(body);
-  }
+    @GetMapping("/health")
+    public ResponseEntity<String> userProgressHealth() {
+       return ResponseEntity.ok("RUNNING");
+    }
 }
