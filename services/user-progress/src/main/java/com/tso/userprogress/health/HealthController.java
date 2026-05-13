8241d3ebe5157cdc4d43e4b39f6f8b8b6ce33f76
Leo Pahl <leo.pahl@tum.de>
2026-05-13 19:59:59 +0200

    Revert "fix: update HealthController to return simple health status"
    This reverts commit 98bbf045ffdb9e0a3615dd0775732a046dc0e377.

--- services/user-progress/src/main/java/com/tso/userprogress/health/HealthController.java
+++ services/user-progress/src/main/java/com/tso/userprogress/health/HealthController.java
@@ -1,15 +1,17 @@
 package com.tso.userprogress.health;
 
-
+import com.tso.userprogress.api.UserProgressApi;
+import com.tso.userprogress.model.HealthStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-class HealthController {
+class HealthController implements UserProgressApi {
 
-    @GetMapping("/health")
-    public ResponseEntity<String> userProgressHealth() {
-       return ResponseEntity.ok("RUNNING");
-    }
+  @Override
+  public ResponseEntity<HealthStatus> userProgressHealth() {
+    HealthStatus body =
+        new HealthStatus().status(HealthStatus.StatusEnum.OK).service("user-progress");
+    return ResponseEntity.ok(body);
+  }
 }
